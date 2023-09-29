d851fac53912a5f95bc124d61f7020040d216670
Joseph Seger <joseph_seger@yahoo.com>
2023-09-29 11:32:56 -0600

    Updated Package

---
+++ Editor/SingletonScriptableObjectAssetNameMatcher.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using JSeger.Utilities.ScriptableObjects.Runtime;
+using UnityEditor;
+using UnityEngine;
+
+namespace JSeger.Utilities.ScriptableObjects.Editor
+{
+    public class SingletonScriptableObjectAssetNameMatcher : AssetPostprocessor
+    {
+        private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+        {
+            foreach (var newPath in movedAssets)
+            {
+                // Check if the asset is a ScriptableObject and implements the ISingletonScriptableObject interface
+                var obj = AssetDatabase.LoadAssetAtPath<ScriptableObject>(newPath);
+
+                if (obj == null || obj is not ISingletonScriptableObject) continue;
+                // If the name does not match the type, revert the name
+                if (obj.name == obj.GetType().Name) continue;
+                var correctPath = Path.GetDirectoryName(newPath) + "/" + obj.GetType().Name + ".asset";
+                AssetDatabase.MoveAsset(newPath, correctPath);
+                Debug.LogError($"You cannot change the name of {obj.GetType().Name}. It has been reverted back.");
+            }
+        }
+    }
+}
---
+++ Runtime/SingletonScriptableObject.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace JSeger.Utilities.ScriptableObjects.Runtime
+{
+    public interface ISingletonScriptableObject
+    {
+        string ResourcePath();
+    }
+
+    public abstract class SingletonScriptableObject<T> : ScriptableObject
+        where T : ScriptableObject, ISingletonScriptableObject
+    {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance != null) return _instance;
+                var tempInstance = CreateInstance<T>();
+                var resourcePath = tempInstance.ResourcePath() + typeof(T).Name;
+                DestroyImmediate(tempInstance);
+
+                _instance = Resources.Load<T>(resourcePath);
+                if (_instance == null)
+                {
+                    Debug.LogError($"Failed to load {typeof(T).Name} from path: {resourcePath}");
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
